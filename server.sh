f80c0e5bb4ee4019ff9ad72698537ddf26ea52c3
clarkzjw <i@jinwei.me>
2023-05-04 21:21:42 -0700

    fix dataset path

--- server.sh
+++ server.sh
@@ -1,2 +1,2 @@
 export SSLKEYLOGFILE="./tmp/key"
-./tserver -M 2 -q qlog -c ./ca-cert.pem -k ./ca-key.pem -p 4443 -w ~/clarkzjw-globecom23/mpd
+./tserver -M 2 -q qlog -c ./ca-cert.pem -k ./ca-key.pem -p 4443 -w ../mpd
