90abbce6e0a5ba64a0b56510742939d0de9db030
clarkzjw <i@jinwei.me>
2023-05-04 21:42:32 -0700

    add results

---
+++ analysis/calc_average_bitrate.py
@@ -0,0 +1,148 @@
+import glob
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cycler
+from collections import defaultdict
+
+alg_list = [
+    # "gcloud_lints",
+    # "gcloud_linucb",
+    "LinTS",
+    "LinUCB",
+    "RR",
+    "minRTT"
+    # "gcloud-minrtt",
+    # "gcloud-rr"
+]
+
+
+param_list = [
+    0.2,
+    0.4,
+    0.6,
+    0.8,
+    1.0,
+]
+
+"""
+Average bitrate CDF
+"""
+
+
+def average_bitrate(filename: str) -> float:
+    with open(filename, 'r') as f:
+        history = json.load(f)
+        history = sorted(history, key=lambda d: d['seg_no'])
+
+        bitrate = []
+
+        for i in range(len(history)):
+            if history[i]["initial_explore"] == 0:
+                bitrate.append(history[i]['bitrate'])
+
+        return sum(bitrate) / len(bitrate)
+
+
+def calc_avg_bitrate_cdf(algorithm: str, parameter: float):
+    if algorithm not in alg_list:
+        print("results for algorithm " + algorithm + " not found")
+        return
+    dir = "../result/mininet_context_0429/{}/".format(algorithm)
+    avg_bitrate = []
+    for filename in glob.glob(dir + "*.json"):
+        if parameter != -1:
+            if str(parameter) in filename and "rebuffering_history_" not in filename:
+                avg_bitrate.append(average_bitrate(filename))
+        else:
+            if "rebuffering_history_" not in filename:
+                avg_bitrate.append(average_bitrate(filename))
+
+    return avg_bitrate
+
+
+def calc_average_bitrate():
+    for alg in alg_list:
+        if alg in ["gcloud_lints", "gcloud_linucb", "LinTS", "LinUCB"]:
+            for alpha in param_list:
+                avg_bitrate_list = calc_avg_bitrate_cdf(alg, alpha)
+                print(avg_bitrate_list)
+                with open("../result/mininet_context_0429/{}-{}-average-bitrate.csv".format(alg, alpha), "w") as f:
+                    for item in avg_bitrate_list:
+                        f.write("%s\n" % item)
+                print("done")
+        else:
+            avg_bitrate_list = calc_avg_bitrate_cdf(alg, -1)
+            print(avg_bitrate_list)
+            with open("../result/mininet_context_0429/{}-average-bitrate.csv".format(alg), "w") as f:
+                for item in avg_bitrate_list:
+                    f.write("%s\n" % item)
+            print("done")
+
+
+def rebuffering_count(filename: str) -> int:
+    with open(filename, 'r') as f:
+        history = json.load(f)
+        history = sorted(history, key=lambda d: d['seg_no'])
+
+        rebuffering = 0
+
+        for i in range(len(history)):
+            if history[i]["initial_explore"] == 0:
+                if history[i]['rebuffering_ratio_after'] - history[i]['rebuffering_ratio'] > 0:
+                    rebuffering += 1
+
+        return rebuffering
+
+def rebuffering_count_new(filename: str) -> int:
+    with open(filename, 'r') as f:
+        history = json.load(f)
+        rebuffering = 0
+
+        for i in range(len(history)):
+            # if history[i]["next_seg_no"] > 12:
+            rebuffering += 1
+
+        return rebuffering
+
+
+def calc_rebuffering_count_cdf(algorithm: str, parameter: float):
+    if algorithm not in alg_list:
+        print("results for algorithm " + algorithm + " not found")
+        return
+    dir = "../result/mininet_context_0429/{}/".format(algorithm)
+    rebuffering_events_count = []
+    for filename in glob.glob(dir + "*.json"):
+        # print(filename)
+        if parameter != -1:
+            if str(parameter) in filename and "rebuffering_history_" not in filename:
+                # rebuffering_events_count.append(rebuffering_count_new(filename))
+                rebuffering_events_count.append(rebuffering_count(filename))
+        else:
+            if "rebuffering_history_" not in filename:
+                rebuffering_events_count.append(rebuffering_count(filename))
+    return rebuffering_events_count
+
+
+def calc_rebuffering_count():
+    for alg in alg_list:
+        if alg in ["gcloud_lints", "gcloud_linucb", "LinTS", "LinUCB"]:
+            for alpha in param_list:
+                avg_bitrate_list = calc_rebuffering_count_cdf(alg, alpha)
+                print(avg_bitrate_list)
+                with open("../result/mininet_context_0429/{}-{}-rebuffering-count.csv".format(alg, alpha), "w") as f:
+                    for item in avg_bitrate_list:
+                        f.write("%s\n" % item)
+                print("done")
+        else:
+            avg_bitrate_list = calc_rebuffering_count_cdf(alg, -1)
+            print(avg_bitrate_list)
+            with open("../result/mininet_context_0429/{}-rebuffering-count.csv".format(alg), "w") as f:
+                for item in avg_bitrate_list:
+                    f.write("%s\n" % item)
+            print("done")
+
+
+if __name__ == "__main__":
+    calc_average_bitrate()
+    calc_rebuffering_count()
---
+++ figure/gcloud/figure_gcloud.m
@@ -0,0 +1,109 @@
+clear;
+close all;
+
+% linucb: 1.0
+% lints: 0.4
+
+minRTT_average_bitrate = readmatrix('./minRTT-average-bitrate.csv');
+minRTT_rebuffering = readmatrix('./minRTT-rebuffering-count.csv');
+RR_average_bitrate = readmatrix('./RR-average-bitrate.csv');
+RR_rebuffering = readmatrix('./RR-rebuffering-count.csv');
+
+
+lints_alpha = 0.4;
+linucb_alpha = 1.0;
+
+h = figure('visible','off');
+legend_labels = cell(1, 4);
+
+% average bitrate
+ts_average_bitrate = readmatrix(sprintf("./LinTS-%.1f-average-bitrate.csv", lints_alpha));
+ucb_average_bitrate = readmatrix(sprintf("./LinUCB-%.1f-average-bitrate.csv", linucb_alpha));
+
+subplot(1,2,1);
+
+legend_labels{1} = sprintf('minRTT');
+[F, X] = ecdf(minRTT_average_bitrate);
+plot(X, F, '-', 'LineWidth', 2);
+xlabel('Average bitrate (Kbps)');
+ylabel('CDF');
+hold on;
+
+legend_labels{2} = sprintf('RR');
+[F, X] = ecdf(RR_average_bitrate);
+plot(X, F, '--', 'LineWidth', 2);
+ylabel('CDF');
+xlabel('Average bitrate (Kbps)');
+hold on;
+
+legend_labels{3} = sprintf('LinTS');
+[F, X] = ecdf(ts_average_bitrate);
+p = plot(X, F, ':', 'LineWidth', 2);
+ylabel('CDF');
+xlabel('Average bitrate (Kbps)');
+p.Color = "red";
+hold on;
+
+legend_labels{4} = sprintf('LinUCB');
+[F, X] = ecdf(ucb_average_bitrate);
+plot(X, F, '-.', 'LineWidth', 2);
+ylabel('CDF');
+xlabel('Average bitrate (Kbps)');
+legend(legend_labels, 'Location','best');
+% title('Average bitrate (Kbps)');
+
+% rebuffering
+ts_rebuffering = readmatrix(sprintf("./LinTS-%.1f-rebuffering-count.csv", lints_alpha));
+ucb_rebuffering = readmatrix(sprintf("./LinUCB-%.1f-rebuffering-count.csv", linucb_alpha));
+
+subplot(1,2,2);
+
+legend_labels{1} = sprintf('minRTT');
+[F, X] = ecdf(minRTT_rebuffering);
+plot(X, F, '-', 'LineWidth', 2);
+ylabel('CDF');
+xlabel('Rebuffering event count');
+xlim([0, 9]);
+hold on;
+
+legend_labels{2} = sprintf('RR');
+[F, X] = ecdf(RR_rebuffering);
+plot(X, F, '--', 'LineWidth', 2);
+ylabel('CDF');
+xlabel('Rebuffering event count');
+xlim([0, 9]);
+hold on;
+
+legend_labels{3} = sprintf('LinTS');
+[F, X] = ecdf(ts_rebuffering);
+p = plot(X, F, ':', 'LineWidth', 2);
+ylabel('CDF');
+xlabel('Rebuffering event count');
+p.Color = "red";
+xlim([0, 9]);
+hold on;
+
+legend_labels{4} = sprintf('LinUCB');
+[F, X] = ecdf(ucb_rebuffering);
+plot(X, F, '-.', 'LineWidth', 2);
+ylabel('CDF');
+xlabel('Rebuffering event count');
+xlim([0, 9]);
+legend(legend_labels, 'Location', 'best');
+% title('Rebuffering event count');
+
+% set(h,'Units','Inches');
+% pos = get(h,'Position');
+% set(h,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+% print(h,sprintf('gcloud-lints-%.1f-linucb-%.1f.pdf', lints_alpha, linucb_alpha),'-dpdf','-bestfit','-r0')
+
+f = gcf;
+
+posA = get(f,'Position');
+posA(3) = 1200;
+posA(4) = 300;
+set(f,'Position',posA);
+
+exportgraphics(f,sprintf('mininet-lints-%.1f-linucb-%.1f.pdf', lints_alpha, linucb_alpha))
+
+fprintf("LinTS Alpha: %.1f, LinUCB Alpha: %.1f\n", lints_alpha, linucb_alpha);
